25e1f66f19963bb30b288caa0e770eb9aa4c0a30
gengyun li <gengyunli@gmail.com>
2011-08-19 10:51:47 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Konjac::Application.config.secret_token = '99b7cc42cd514ba8b06088b6f26571c4077ffbee0dafceff966f52392ef284ad81d179b176730282ecb212c206a2e0831a5f4348315b5b6d63c5ba3a8957a133'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Konjac::Application.config.session_store :cookie_store, :key => '_konjac_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Konjac::Application.config.session_store :active_record_store
